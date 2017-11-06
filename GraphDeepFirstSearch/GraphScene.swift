8d913d2e0e005d899c6f2ef9390efa9c78c149bc
Tymofii Dolenko <tymofii@dolenko.com>
2017-11-06 21:21:58 +0200

    Init commit

---
+++ GraphDeepFirstSearch/GraphScene.swift
@@ -0,0 +1,13 @@
+//
+//  GraphScene.swift
+//  GraphDeepFirstSearch
+//
+//  Created by Tymofii Dolenko on 11/6/17.
+//  Copyright © 2017 Tymofii Dolenko. All rights reserved.
+//
+
+import UIKit
+
+class GraphScene: SKScene {
+
+}
---
+++ GraphDeepFirstSearch/MainVC.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  GraphDeepFirstSearch
+//
+//  Created by Tymofii Dolenko on 11/6/17.
+//  Copyright © 2017 Tymofii Dolenko. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class MainVC: UIViewController {
+    
+    @IBOutlet weak var skView: SKView!
+    
+    var scene: GraphScene!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scene = GraphScene(size: self.view.bounds.size)
+        scene.scaleMode = .resizeFill
+        skView.ignoresSiblingOrder = true
+        skView.presentScene(scene)
+    }
+
+
+}
+
